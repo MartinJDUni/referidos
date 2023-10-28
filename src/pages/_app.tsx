9d4c43d830df57039630013edfb3d12e4c03b8fa
Kevstar9-1 <126917986+Kevstar9-1@users.noreply.github.com>
2023-10-28 14:18:39 -0600

    create Login
    create Login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import BootstrapWrapper from './components/BootstrapWrapper';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
+
 function App({ Component, pageProps }) {
   return (
     <BootstrapWrapper> <Component {...pageProps} /></BootstrapWrapper>
---
+++ src/pages/components/login.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import { useRouter } from 'next/router';
+import styles from '@/styles/Login.module.css';
+
+const Login: FC = () => {
+  const router = useRouter();
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Aquí se pretende hacer la autenticación
+    router.push('/Main');
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles['login-form']}>
+          <img src="./images/logo.png" alt="Logo-login" className={styles['logo-login']} />
+          <h2>Inicio de Sesión</h2>
+          <form onSubmit={handleLogin}>
+            <input className={styles.input} type="text" placeholder="Username" />
+            <input className={styles.input} type="password" placeholder="Password" />
+            <div className={styles.buttonContainer}>
+              <button className={styles.button} type="submit">
+                Ingresar
+              </button>
+              <button className={styles.registerButton} type="submit">
+                Registrarse
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
