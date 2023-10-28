9c09e7232471f37ec5986dd2820a66bf25cc65a1
MartinJDUni <128074166+MartinJDUni@users.noreply.github.com>
2023-10-27 22:19:55 -0600

    header updata

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,35 +1,38 @@
-// components/Header.tsx
-
 import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "@/styles/Header.module.css"; // Estilo personalizado
 
 const Header: React.FC = () => {
   return (
-    <header className="navbar navbar-expand-lg bg-light">
+    <header className={`navbar navbar-expand-lg ${styles.header}`}>
       <div className="container">
         <Link href="/" legacyBehavior>
           <a className="navbar-brand">Your Logo</a>
         </Link>
 
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              User
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                <Link href="/profile" legacyBehavior>
-                  <a className="dropdown-item">Profile</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/sign-out" legacyBehavior>
-                  <a className="dropdown-item">Sign out</a>
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <div className={`ml-auto position-relative ${styles.dropdownContainer}`}>
+          <button
+            className="nav-link dropdown-toggle"
+            type="button"
+            id="navbarDropdown"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            User
+          </button>
+          <ul className={`custom-dropdown-menu dropdown-menu ${styles.dropdownMenu}`} aria-labelledby="navbarDropdown">
+            <li>
+              <Link href="/profile" legacyBehavior>
+                <a className="dropdown-item">Profile</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/sign-out" legacyBehavior>
+                <a className="dropdown-item">Sign out</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
