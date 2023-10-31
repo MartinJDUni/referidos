0bb54a9ec9fc5839c3e274e8ac3ba6217f76fb9b
Kevstar9-1 <126917986+Kevstar9-1@users.noreply.github.com>
2023-10-31 11:36:19 -0600

    update Graphics and create databaseEmployeeTask
    update Graphics and create databaseEmployeeTask

--- src/pages/Admin/Graphics.tsx
+++ src/pages/Admin/Graphics.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Layout } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Layout, Modal, Select, DatePicker, Button, Input } from 'antd';
 import HeaderComponent from "@/pages/components/header";
 import SidebarComponent from "@/pages/components/SideBars";
 import PieChart from '../components/graPie';
@@ -7,19 +7,90 @@ import DataGridPremiumDemo from '@/pages/components/tablaET';
 import BarChart from '../components/graBar';
 
 const { Header, Content } = Layout;
+const { Option } = Select;
 
 const Graphic: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] = useState('');
+  const [selectedTask, setSelectedTask] = useState('');
+  const [goal, setGoal] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [employeeOptions, setEmployeeOptions] = useState([]);
+  const [taskOptions, setTaskOptions] = useState([]);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleOk = async () => {
+    try {
+      const response = await fetch('/api/databaseEmployeeTask', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Idemployee: selectedEmployee,
+          Idtask: selectedTask,
+          Goal: goal,
+          Startdate: startDate,
+          Finaldate: endDate,
+          state: 1, // Define el estado deseado aquí
+        }),
+      });
+
+      if (response.ok) {
+        // Manejar la respuesta exitosa si es necesario
+        console.log('Datos de la meta asignados con éxito');
+      } else {
+        console.error('Error al enviar los datos del modal al servidor');
+      }
+    } catch (error) {
+      console.error('Error al enviar los datos del modal al servidor:', error);
+    }
+
+    setIsModalVisible(false);
+  };
 
   const handleToggleSidebar = () => {
     setCollapsed(!collapsed);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Reemplaza esta URL con la dirección correcta de tu API
+        const response = await fetch('/api/databaseEmployeeTask');
+        if (response.ok) {
+          const data = await response.json();
+          setEmployeeOptions(data.employees);
+          setTaskOptions(data.tasks);
+        } else {
+          console.error('Error al obtener la lista de empleados y tareas');
+        }
+      } catch (error) {
+        console.error('Error al obtener la lista de empleados y tareas:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const pieChartData = [12, 19, 3, 17, 6];
   const pieChartLabels = ['Manzanas', 'Plátanos', 'Uvas', 'Naranjas', 'Cerezas'];
 
   const barChartData = [10, 15, 8, 25, 12];
   const barChartLabels = ['Producto A', 'Producto B', 'Producto C', 'Producto D', 'Producto E'];
+
+  const fieldStyle = {
+    marginBottom: '15px',
+  };
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
@@ -37,9 +108,68 @@ const Graphic: React.FC = () => {
             <div className="chart">
               <PieChart data={pieChartData} labels={pieChartLabels} />
             </div>
+<<<<<<< Updated upstream
             <div>
               <BarChart/>
             </div>
+=======
+            <Button type="primary" onClick={showModal}>
+              Asignar meta
+            </Button>
+            <Modal
+              title="Asignar meta"
+              visible={isModalVisible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <div style={fieldStyle}>
+                <label>Meta:</label>
+                <Input value={goal} onChange={(e) => setGoal(e.target.value)} />
+              </div>
+              <div style={fieldStyle}>
+                <label>Empleado:</label>
+                <Select
+                  style={{ width: 200 }}
+                  value={selectedEmployee}
+                  onChange={(value) => setSelectedEmployee(value)}
+                >
+                  <Option value="" disabled>
+                    Selecciona un empleado
+                  </Option>
+                  {employeeOptions.map((employee) => (
+                    <Option key={employee.Id} value={employee.Id.toString()}>
+                      {employee.Name}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <div style={fieldStyle}>
+                <label>Nombre de la tarea:</label>
+                <Select
+                  style={{ width: 200 }}
+                  value={selectedTask}
+                  onChange={(value) => setSelectedTask(value)}
+                >
+                  <Option value="" disabled>
+                    Selecciona una tarea
+                  </Option>
+                  {taskOptions.map((task, index) => (
+                    <Option key={index} value={task.Id}>
+                      {task.Name}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <div style={fieldStyle}>
+                <label>Fecha de inicio:</label>
+                <DatePicker value={startDate} onChange={(date) => setStartDate(date)} />
+              </div>
+              <div style={fieldStyle}>
+                <label>Fecha final:</label>
+                <DatePicker value={endDate} onChange={(date) => setEndDate(date)} />
+              </div>
+            </Modal>
+>>>>>>> Stashed changes
           </div>
           <div className="data-grid">
             <DataGridPremiumDemo />
---
+++ src/pages/api/databaseEmployeeTask.ts
@@ -0,0 +1,68 @@
+import { createConnection } from 'mysql2/promise';
+
+export async function connectToDatabase() {
+    const connection = await createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '', // Cambia esto por la contraseña de tu base de datos
+        database: 'referidos', // Cambia esto por el nombre de tu base de datos
+    });
+    return connection;
+}
+
+export default async (req, res) => {
+    if (req.method === 'GET') {
+        const connection = await connectToDatabase();
+
+        try {
+            // Realiza la consulta SQL para obtener solo el ID del empleado
+            const employeeQuery = `
+                SELECT Id
+                FROM employee;
+            `;
+            const [employeeRows] = await connection.execute(employeeQuery);
+
+            // SQL query to fetch task data
+            const taskQuery = `
+                SELECT Id, Name
+                FROM task;
+            `;
+            const [taskRows] = await connection.execute(taskQuery);
+
+            connection.end();
+
+            res.status(200).json({
+                employees: employeeRows,
+                tasks: taskRows,
+            });
+        } catch (error) {
+            console.error('Error al consultar la base de datos:', error);
+            res.status(500).json({ error: 'Error al consultar la base de datos' });
+        }
+    } else if (req.method === 'POST') {
+        const { Idemployee, Idtask, Goal, Startdate, Finaldate, state } = req.body; // Asume que estás enviando estos datos en el cuerpo de la solicitud POST.
+    
+        if (!Idemployee || !Idtask || !Goal || !Startdate || !Finaldate || state === undefined) {
+            res.status(400).json({ error: 'Faltan datos obligatorios' });
+            return;
+        }
+    
+        const connection = await connectToDatabase();
+    
+        try {
+            // Realiza la inserción de datos en la base de datos
+            const [result] = await connection.execute(
+                'INSERT INTO employeespertask (Idemployee, Idtask, Goal, Startdate, Finaldate, state) VALUES (?, ?, ?, ?, ?, ?)',
+                [Idemployee, Idtask, Goal, Startdate, Finaldate, state]
+            );
+    
+            connection.end();
+    
+            res.status(201).json({ message: 'Datos insertados correctamente', insertedId: result.insertId });
+        } catch (error) {
+            console.error('Error al insertar datos en la base de datos:', error);
+            res.status(500).json({ error: 'Error al insertar datos en la base de datos' });
+        }
+    }
+    
+};
