6f86825c51bcafa870e9065b4373f4eda7536f49
MartinJDUni <128074166+MartinJDUni@users.noreply.github.com>
2024-04-24 21:44:33 -0600

    test

--- src/pages/components/listComent.tsx
+++ src/pages/components/listComent.tsx
@@ -175,6 +175,7 @@ const CommentList = ({ id }: { id: number }) => {
       <Modal open={isModalOpen} onClose={closeModal}>
         <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
           <TextField
+          id='Ag'
             label="Nuevo Comentario"
             multiline
             rows={4}
