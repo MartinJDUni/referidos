493d60cfb0954dad77109c5d0b944bdeb03297d2
Kevstar9-1 <126917986+Kevstar9-1@users.noreply.github.com>
2024-05-13 16:38:49 -0600

    Revert "Merge branch 'main' of https://github.com/MartinJDUni/referidos"
    This reverts commit 92d75a19e590df7e879778745216ac2043b34ac4, reversing
    changes made to 2dfbc38378e3ba20c68a2b23c5570e481827beb5.

--- src/pages/components/tablaET.tsx
+++ src/pages/components/tablaET.tsx
@@ -5,22 +5,26 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-interface RowData {
-  id: number;
-  nombre_empleado: string;
-  rol_empleado: string;
-  total_tareas: number;
-  tareas_aceptadas: number;
-  state: number;
+interface EditedFields {
+  goal?: string; // O el tipo correcto para el campo goal
+  start?: Date;
+  final?: Date;
 }
 
 export default function DataGridPremiumDemo() {
   const [data, setData] = React.useState<any[]>([]);
   const [loading, setLoading] = React.useState(true);
+  const [showStateZero, setShowStateZero] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const [editingRowId, setEditingRowId] = React.useState<any>(null);
-  const [editedFields, setEditedFields] = React.useState<any>({});
+  const [editedFields, setEditedFields] = React.useState<EditedFields>({});
   const [rowSelectionModel, setRowSelectionModel] = React.useState<any[]>([]);
 
   const getProgressColor = (value: number) => {
@@ -29,7 +33,7 @@ export default function DataGridPremiumDemo() {
         background: '#E53935', // Rojo para 0-30
         bar: '#C62828',
       };
-    } else if (value < 80) {
+    } else if (value < 100) {
       return {
         background: '#FFC107', // Amarillo para 30-99
         bar: '#FFA000',
@@ -75,18 +79,18 @@ export default function DataGridPremiumDemo() {
       ),
     },
     { field: 'id', headerName: 'Id', width: 50 },
-    { field: 'nombre_empleado', headerName: 'Nombre', width: 150 },
-    { field: 'rol_empleado', headerName: 'Tarea', width: 150 },
-    { field: 'total_tareas', headerName: 'Meta', width: 80 },
+    { field: 'Ename', headerName: 'Nombre', width: 150 },
+    { field: 'Tname', headerName: 'Tarea', width: 150 },
+    { field: 'goal', headerName: 'Meta', width: 80 },
     {
-      field: 'tareas_aceptadas',
+      field: 'TaskCount',
       headerName: 'Completados',
       headerAlign: 'center',
       align: 'center',
       width: 200,
       renderCell: (params) => {
         const { background, bar } = getProgressColor(params.value || 0);
-
+  
         return (
           <Box display="flex" alignItems="center" sx={{ width: '100%' }}>
             <LinearProgress
@@ -103,12 +107,41 @@ export default function DataGridPremiumDemo() {
               }}
             />
             <Typography variant="body2" color="textSecondary" sx={{ marginLeft: 1 }}>
-            {typeof params.value === 'number' ? `${params.value.toFixed(2)}%` : ''}
+              {`${params.value.toFixed(2)}%`}
             </Typography>
           </Box>
         );
       },
     },
+    {
+      field: 'start',
+      headerName: 'Fecha de inicio',
+      headerAlign: 'center',
+      align: 'center',
+      width: 150,
+      valueFormatter: (params) => {
+        const date = new Date(params.value);
+        const year = date.getFullYear();
+        const month = `0${date.getMonth() + 1}`.slice(-2);
+        const day = `0${date.getDate()}`.slice(-2);
+        return `${year}-${month}-${day}`;
+      },
+    },
+    {
+      field: 'final',
+      headerName: 'Fecha de final',
+      headerAlign: 'center',
+      align: 'center',
+      width: 150,
+      valueFormatter: (params) => {
+        const date = new Date(params.value);
+        const year = date.getFullYear();
+        const month = `0${date.getMonth() + 1}`.slice(-2);
+        const day = `0${date.getDate()}`.slice(-2);
+        return `${year}-${month}-${day}`;
+      },
+    },
+    { field: 'state', headerName: 'Estado', width: 100 },
   ];
 
   const handleEditRow = (id: any) => {
@@ -188,27 +221,54 @@ export default function DataGridPremiumDemo() {
     }
   };
 
-  const fetchData = React.useCallback(() => {
-    fetch('/api/databaseET')
-      .then((response) => response.json())
-      .then((resultET) => {
-        const mappedDataET = resultET.data.map((row: any) => ({
-          id: row.id,
-          nombre_empleado: row.nombre_empleado,
-          rol_empleado: row.rol_empleado,
-          total_tareas: row.total_tareas,
-          tareas_aceptadas: (row.tareas_aceptadas / (row.total_tareas / 0.8)) * 100,
+  const fetchData = () => {
+    const promises = [
+      fetch('/api/databaseET').then((response) => response.json()),
+      fetch('/api/dbC').then((response) => response.json()),
+    ];
+
+    Promise.all(promises)
+      .then((results) => {
+        const [resultET, resultOtherAPI] = results;
+
+        const filteredDataET = resultET.data.filter((row: { state: number; }) =>
+          showStateZero ? row.state === 0 : row.state === 1
+        );
+
+        const mappedDataET = filteredDataET.map((row: { Id: any; EmployeeId: any; EmployeeName: any; TaskName: any; Goal: any; Startdate: string | number | Date; Finaldate: string | number | Date; state: any; }) => ({
+          id: row.Id,
+          EmployeeId: row.EmployeeId,
+          Ename: row.EmployeeName,
+          Tname: row.TaskName,
+          goal: row.Goal,
+          start: new Date(row.Startdate),
+          final: new Date(row.Finaldate),
           state: row.state,
         }));
-        console.log(resultET.data);
-        setData(mappedDataET);
+
+        const finalData = mappedDataET.map((rowET: { EmployeeId: any; goal: number; }) => {
+          const matchingOtherData = resultOtherAPI.data.find((otherRow: { EmployeeId: any; }) => {
+            return otherRow.EmployeeId === rowET.EmployeeId;
+          });
+
+          const taskCount = matchingOtherData?.TaskCount || 0;
+          const goal = rowET.goal || 1;
+
+          return {
+            ...rowET,
+            TaskCount: (taskCount / goal) * 100,
+            TaskIds: matchingOtherData?.TaskIds || [],
+          };
+        });
+
+        setData(finalData);
         setLoading(false);
       })
       .catch((error) => {
         console.error('Error al obtener datos de la base de datos:', error);
         setLoading(false);
       });
-  }, []);
+  };
 
   React.useEffect(() => {
     fetchData();
@@ -219,15 +279,24 @@ export default function DataGridPremiumDemo() {
     return () => {
       clearInterval(pollingInterval);
     };
-  },);
+  }, [showStateZero]);
 
   return (
     <Box sx={{ height: 520, width: '100%' }}>
+      <div>
+        <button
+          onClick={() => setShowStateZero(!showStateZero)}
+          style={{ backgroundColor: showStateZero ? 'green' : 'red', color: 'white' }}
+        >
+          {showStateZero ? ' Ver Activos' : 'Ver Inactivos'}
+        </button>
+      </div>
       <DataGrid
         rows={data}
         columns={columns}
         loading={loading}
         rowSelectionModel={rowSelectionModel}
+
         components={{
           Toolbar: (props) => (
             <div>
@@ -235,8 +304,49 @@ export default function DataGridPremiumDemo() {
             </div>
           ),
         }}
+
         getRowId={(row) => row.id}
       />
+
+      <Dialog open={open} onClose={handleEditClose} fullWidth maxWidth="xs">
+        <DialogTitle>Editar Información</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Meta"
+            fullWidth
+            margin="normal"
+            style={{ marginBottom: 16 }}
+            value={editedFields.goal || ''}
+            onChange={(e) => setEditedFields({ ...editedFields, goal: e.target.value })}
+          />
+          <TextField
+            label="Fecha de inicio"
+            type="date"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            style={{ marginBottom: 16 }}
+            value={editedFields.start ? editedFields.start.toISOString().split('T')[0] : ''}
+            onChange={(e) => setEditedFields({ ...editedFields, start: new Date(e.target.value) })}
+          />
+          <TextField
+            label="Fecha de final"
+            type="date"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            style={{ marginBottom: 16 }}
+            value={editedFields.final ? editedFields.final.toISOString().split('T')[0] : ''}
+            onChange={(e) => setEditedFields({ ...editedFields, final: new Date(e.target.value) })}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleEditClose} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleSaveEdit} color="primary">
+            Guardar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
